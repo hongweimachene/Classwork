d1cf02eef02b13cf2f43a2c37389bb48f0af3f7a
hongweimachene <hchen00@stuy.edu>
2019-02-04 11:45:04 -0500

    started classwork

---
+++ makeAllWords.java
@@ -0,0 +1,11 @@
+import java.util.List;
+public class makeAllWords{
+  public static List<String> makeAllWords(int k, int maxLetter){
+    List<String> f = new List<String>();
+    return makeAllWordsH(k, "", maxLetter, f, 'a');
+  }
+  private static List<String> makeAllWordsH(int k, String word, int maxLetter, List<String> list, char letter){
+    
+    }
+
+  }
