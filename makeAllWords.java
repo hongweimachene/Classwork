564666c313bc741b63ac2bb3bfa107565a021e44
hongweimachene <hchen00@stuy.edu>
2019-02-05 22:37:29 -0500

    updated makeAllWords

--- makeAllWords.java
+++ makeAllWords.java
@@ -1,11 +1,16 @@
 import java.util.List;
+import java.util.ArrayList;
 public class makeAllWords{
   public static List<String> makeAllWords(int k, int maxLetter){
-    List<String> f = new List<String>();
+    List<String> f = new ArrayList<String>();
     return makeAllWordsH(k, "", maxLetter, f, 'a');
   }
-  private static List<String> makeAllWordsH(int k, String word, int maxLetter, List<String> list, char letter){
-    
+  private static void makeAllWordsH(int k, String word, int maxLetter, List<String> list, char letter){
+    if (k == 0) {
+      list.add(word);
+    } else {
+      for (int i = 0; i < maxLetter; i++) {
+        makeAllWords(k-1, word+letter, maxLetter, list, letter+i);
+      }
     }
-
   }
