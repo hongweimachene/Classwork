c1eb855360d6bf21a11a585d47d2e018bb49c031
hongweimachene <hchen00@stuy.edu>
2019-02-05 23:02:59 -0500

    fixed makeAllWords, works

--- makeAllWords.java
+++ makeAllWords.java
@@ -3,14 +3,21 @@ import java.util.ArrayList;
 public class makeAllWords{
   public static List<String> makeAllWords(int k, int maxLetter){
     List<String> f = new ArrayList<String>();
-    return makeAllWordsH(k, "", maxLetter, f, 'a');
+    makeAllWordsH(k, "", maxLetter, f, 97);
+    return f;
   }
-  private static void makeAllWordsH(int k, String word, int maxLetter, List<String> list, char letter){
+  private static void makeAllWordsH(int k, String word, int maxLetter, List<String> list, int letter){
     if (k == 0) {
       list.add(word);
     } else {
-      for (int i = 0; i < maxLetter; i++) {
-        makeAllWords(k-1, word+letter, maxLetter, list, letter+i);
+      for (int i = 0 ; i < maxLetter; i++) {
+        makeAllWordsH(k-1, word+(char) (letter+i), maxLetter, list, letter);
       }
     }
   }
+  public static void main(String[] args) {
+    System.out.println(makeAllWords(1,26));
+    System.out.println(makeAllWords(3,3));
+    System.out.println(makeAllWords(4,2));
+  }
+}
